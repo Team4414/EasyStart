cd15713a71252f395ece326a1494b59cf34eb056
ikim5569 <77216801+ikim5569@users.noreply.github.com>
2021-07-01 20:20:10 -0700

    change

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,6 @@
 
 /**
-   * Imports: these are imports from the vender libraries that you should have installed
+   * Imports: these are imports from the vendor libraries that you should have installed
    * You generally don't have to worry about these; VS code will fill them in for you
    * In the case that VS code doesn't import automatically, you can hover over the word and click 
    * "quick fix", which will give you the choice to import manually. 
@@ -17,7 +17,7 @@ public class Robot extends TimedRobot {
 /**
    * Declaration: we are letting the program know that there will be an object of type TalonFX 
    * and that we're naming it "newTalon" (it can be named anything you want). 
-   * The TalonFX is a type of speed controller for the robot motors. 
+   * The TalonFX is a type of speed controller for robot motors. 
    */
 
   TalonFX newTalon; 
@@ -26,16 +26,13 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
 
-    newTalon = new TalonFX(0);
-
   /**
    * Instantiation: the "new" keyword lets the program know that memory is being allocated for our new object
    * The 0 in the parenthesis is the ID number of the motor (we could put any valid ID number and control different 
    * motors)
    */
 
-
-  
+    newTalon = new TalonFX(0);
 
   }
 
@@ -50,11 +47,13 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-      newTalon.set(ControlMode.PercentOutput, 30); 
+  
   /**
    * In teletopInit (when we turn on the teleop mode on Driver Station), we are setting the power of the motor "newTalon"
    * We are using control mode (percent output), and 30 is our output value 
    */
+      newTalon.set(ControlMode.PercentOutput, .3); 
+  
   }
 
   @Override
@@ -65,10 +64,13 @@ public class Robot extends TimedRobot {
 
   @Override
   public void disabledPeriodic() {
-    newTalon.set(ControlMode.PercentOutput, 0); 
+
   /**
    * In disabledPeriodic, we are setting the output value of the motor to 0 periodically. 
    */
+
+    newTalon.set(ControlMode.PercentOutput, 0); 
+  
     
   }
 
